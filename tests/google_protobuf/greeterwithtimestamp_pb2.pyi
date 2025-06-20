7fcab638f55100481494a11f7f36b705e5b6e39e
Jorge Menjivar <jorge@useatlas.ai>
2025-06-19 18:23:30 -0700

    Use current uv.lock grpc ver for test output

--- tests/google_protobuf/greeterwithtimestamp_pb2.pyi
+++ tests/google_protobuf/greeterwithtimestamp_pb2.pyi
@@ -1,8 +1,7 @@
 from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from collections.abc import Mapping as _Mapping
-from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -10,4 +9,4 @@ class WithTimestamp(_message.Message):
     __slots__ = ("timestamp",)
     TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
     timestamp: _timestamp_pb2.Timestamp
-    def __init__(self, timestamp: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
+    def __init__(self, timestamp: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
